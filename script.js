5d9309d73e3e35eb957c5f48a3dd7e8408c9d142
reza hasani <reza.hasani11177@gmail.com>
2022-02-10 18:53:16 +0330

    Create a snake

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const gameCanvas = document.querySelector('canvas');
 let ctx = gameCanvas.getContext('2d');
 ctx.fillStyle = 'white';
-ctx.strokeStyle = 'black';
 ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
 ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+
+let snake = [
+    { x: 150, y: 150 },
+    { x: 140, y: 150 },
+    { x: 130, y: 150 },
+    { x: 120, y: 150 },
+    { x: 110, y: 150 }
+];
+snake.forEach(snakePart => {
+    ctx.fillStyle = 'lightgreen';
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+})
